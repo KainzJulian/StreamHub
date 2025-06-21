34f8ea19ec8e28b365678f6b2a1ed8cbc28197d3
JulianK123 <juliankainz8@gmail.com>
2025-06-21 21:30:02 +0200

    fix(frontend): removed method getMediaType from Media class

--- frontend/src/shared/types/movie.ts
+++ frontend/src/shared/types/movie.ts
@@ -3,15 +3,11 @@ import { Media } from './media';
 export class Movie extends Media {
   pathMedia: string | null;
 
-  override type: string;
+  override readonly type: string;
 
   constructor(data: Partial<Movie>) {
     super(data);
     this.pathMedia = data.pathMedia ?? null;
     this.type = 'Movie';
-  }
-
-  public override getMediaType(): string {
-    return 'Movie';
   }
 }
