dbfcf67cd99984513b361705a14742df2593d00f
JulianK123 <juliankainz8@gmail.com>
2025-06-28 11:12:00 +0200

    feat(ui): improve movie page #16

--- frontend/src/shared/components/pages/movie-page/movie-page.ts
+++ frontend/src/shared/components/pages/movie-page/movie-page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { MediaTemplate } from '../../templates/media-template/media-template';
 import { HomeTemplate } from '../../templates/home-template/home-template';
 import { Media } from '../../../types/media';
@@ -8,6 +8,8 @@ import { ActivatedRoute } from '@angular/router';
 import { CurrentMediaRoutes } from '../../../../utils/apiRoutes';
 import { Movie } from '../../../types/movie';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { MediaCard } from '../../atoms/media-card/media-card';
 import { MediaCardList } from '../../molecules/media-card-list/media-card-list';
 
 @Component({
@@ -18,28 +20,37 @@ import { MediaCardList } from '../../molecules/media-card-list/media-card-list';
     MediaTemplate,
     HomeTemplate,
     GenreList,
+    MediaCard,
     MediaCardList,
   ],
   templateUrl: './movie-page.html',
   styleUrl: './movie-page.scss',
 })
-export class MoviePage implements OnInit {
+export class MoviePage implements OnInit, OnDestroy {
   currentMovie = signal<Media | null>(null);
   similarMovies = signal<Media[] | null>(null);
+
+  private subscribtion!: Subscription;
 
   constructor(
     public mediaService: MediaService,
     private route: ActivatedRoute
   ) {}
 
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
+  }
+
   ngOnInit(): void {
-    const movieID = this.route.snapshot.paramMap.get('id');
+    this.subscribtion = this.route.paramMap.subscribe((data) => {
+      if (data === null) return;
+      const movieID = data.get('id');
+      if (movieID === null) throw new Error('No Movie ID specified');
 
-    if (!movieID) throw new Error('No ID specified');
-
-    this.mediaService.getMovie(movieID).subscribe((response) => {
-      this.currentMovie.set(new Movie(response.data));
-      this.setSimilarMovies();
+      this.mediaService.getMovie(movieID).subscribe((response) => {
+        this.currentMovie.set(new Movie(response.data));
+        this.setSimilarMovies();
+      });
     });
   }
 
@@ -58,5 +69,14 @@ export class MoviePage implements OnInit {
     this.mediaService.getSimilarMovies(movie.id).subscribe((response) => {
       this.similarMovies.set(response.data);
     });
+  }
+
+  displaySimilarMovies(): boolean {
+    const similarMovies = this.similarMovies();
+
+    if (similarMovies?.length === 1)
+      return similarMovies[0].id !== this.currentMovie()?.id;
+
+    return similarMovies !== null && similarMovies.length > 0;
   }
 }
