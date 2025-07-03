de8d15154b7fdd8efc71f9c3723e8adc220e2ebb
JulianK123 <juliankainz8@gmail.com>
2025-07-03 21:17:27 +0200

    fix(frontend): episodes in the library could not be opened

--- frontend/src/shared/components/pages/media-library/media-library.ts
+++ frontend/src/shared/components/pages/media-library/media-library.ts
@@ -4,6 +4,9 @@ import { MediaService } from '../../../services/media.service';
 import { ActivatedRoute } from '@angular/router';
 import { Media } from '../../../types/media';
 import { MediaCardList } from '../../molecules/media-card-list/media-card-list';
+import { Episode } from '../../../types/seriesEpisode';
+import { Series } from '../../../types/series';
+import { Movie } from '../../../types/movie';
 
 @Component({
   selector: 'media-library',
@@ -26,36 +29,46 @@ export class MediaLibrary {
         case 'highRated':
           this.mediaService
             .getHighestRated(0)
-            .subscribe((response) => this.media.set(response.data));
+            .subscribe((response) => this.setMedia(response.data));
           break;
 
         case 'movies':
           this.mediaService
             .getRandomMovie(0)
-            .subscribe((response) => this.media.set(response.data));
+            .subscribe((response) => this.setMedia(response.data));
           break;
 
         case 'moviesSeries':
           this.mediaService
             .getRandomMediaList(0)
-            .subscribe((response) => this.media.set(response.data));
+            .subscribe((response) => this.setMedia(response.data));
           break;
 
         case 'recWatched':
           this.mediaService
             .getWatchHistory(0)
-            .subscribe((response) => this.media.set(response.data));
+            .subscribe((response) => this.setMedia(response.data));
           break;
 
         case 'series':
           this.mediaService
             .getRandomSeries(0)
-            .subscribe((response) => this.media.set(response.data));
+            .subscribe((response) => this.setMedia(response.data));
           break;
 
         default:
           throw new Error('Type does not Exist: ' + type);
       }
     });
+  }
+
+  private setMedia(media: Media[]) {
+    this.media.set(
+      media.map((media) => {
+        if (media.type === 'Episode') return new Episode(media);
+        if (media.type === 'Movie') return new Movie(media);
+        return new Series(media);
+      })
+    );
   }
 }
