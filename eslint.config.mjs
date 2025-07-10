0db71c21837f515ddf677950d192e761ed87fdb6
JulianK123 <juliankainz8@gmail.com>
2025-07-10 21:56:29 +0200

    fix(build): @styles stylesheet will not import when building the frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...storybook.configs["flat/recommended"],
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs["flat/recommended"],
 ];
