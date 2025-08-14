25224df7a26a4c9b818e9551ffe65f1445418c63
KainzJulian <kainzj9@gmail.com>
2025-08-14 10:06:01 +0200

    test(build): update config for running jest tests for angular components

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,29 +1,11 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: ".",
-  testRegex: ".*\\.spec\\.ts$",
-  transform: {
-    "^.+\\.(t|j)s$": "jest-preset-angular",
-  },
-  collectCoverageFrom: ["**/*.(t|j)s"],
-  coverageDirectory: "../coverage",
-  testEnvironment: "jsdom",
-  testTimeout: 20000, // slower machines need longer, than default 5 seconds to create test db.
-  moduleNameMapper: {
-    "^@entities/(.*)$": "<rootDir>/entities/$1",
-    "^@app/(.*)$": "<rootDir>/app/$1",
-    "^@resources/(.*)$": "<rootDir>/resources/$1",
-    "^@constants/(.*)$": "<rootDir>/constants/$1",
-    "^@dbConnection/(.*)$": "<rootDir>/db-connection/$1",
-    "^@daos/(.*)$": "<rootDir>/daos/$1",
-    "^@interfaces/(.*)$": "<rootDir>/interfaces/$1",
-    "^@objTypes/(.*)$": "<rootDir>/types/$1",
-    "^@utils/(.*)$": "<rootDir>/utils/$1",
-    "^@tests/(.*)$": "<rootDir>/test/$1",
-    "^@common/(.*)$": "<rootDir>/../../common/$1",
-  },
   preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["./setup-jest.ts"],
-  verbose: false,
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  transform: {
+    "^.+\\.ts$": "ts-jest", // Only transform .ts files
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!flat)/", // Exclude modules except 'flat' from transformation
+  ],
 };
--- frontend/setup-jest.ts
+++ frontend/setup-jest.ts
@@ -1,15 +1,2 @@
-import { TestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
-import 'zone.js';
-
-function setupJEST() {
-  TestBed.initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
-  );
-}
-
-setupJEST();
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+setupZoneTestEnv();
