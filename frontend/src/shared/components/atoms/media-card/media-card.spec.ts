934fee156cb4b463e672d5ce7dada049b4bf7533
KainzJulian <kainzj9@gmail.com>
2025-08-18 21:24:40 +0200

    test(frontend): media-card Frontend Atoms #35

--- frontend/src/shared/components/atoms/media-card/media-card.spec.ts
+++ frontend/src/shared/components/atoms/media-card/media-card.spec.ts
@@ -1,23 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MediaCard } from './media-card';
+import { MediaRouterService } from '../../../services/media-router.service';
+import { MediaService } from '../../../services/media.service';
+import { Media } from '../../../types/media';
+import { signal } from '@angular/core';
+import { mockMovie } from '../../../mock/mock.data';
 
 describe('MediaCard', () => {
   let component: MediaCard;
   let fixture: ComponentFixture<MediaCard>;
 
+  let mediaRouterServiceMock: jest.Mocked<MediaRouterService>;
+  let mediaServiceMock: jest.Mocked<MediaService>;
+
+  const currentMediaMock = signal<Media | null>(null);
+
   beforeEach(async () => {
+    mediaRouterServiceMock = {
+      openMediaPlayer: jest.fn(),
+    } as unknown as jest.Mocked<MediaRouterService>;
+
+    mediaServiceMock = {
+      currentMedia: currentMediaMock,
+    } as unknown as jest.Mocked<MediaService>;
+
     await TestBed.configureTestingModule({
-      imports: [MediaCard]
-    })
-    .compileComponents();
+      imports: [MediaCard],
+      providers: [
+        {
+          provide: MediaRouterService,
+          useValue: mediaRouterServiceMock,
+        },
+        {
+          provide: MediaService,
+          useValue: mediaServiceMock,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MediaCard);
     component = fixture.componentInstance;
+
+    component.media = mockMovie;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('openPlayer', () => {
+    it('should throw an Error: Media not set', () => {
+      component.media = null;
+      fixture.detectChanges();
+
+      expect(() => component.openPlayer()).toThrow('Media not set');
+    });
+
+    it('should ', () => {});
   });
 });
